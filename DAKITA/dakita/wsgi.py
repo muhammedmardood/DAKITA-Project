872617e9f42d6b91ca04d9d64401ddf2aad59ad6
muhammed mardood <muhammedmardood@gmail.com>
2021-11-14 22:55:35 +0300

    first commit

--- DAKITA/dakita/wsgi.py
+++ DAKITA/dakita/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tccomment project.
+WSGI config for dakita project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tccomment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dakita.settings')
 
 application = get_wsgi_application()
